982be9ec6a9e6e9aa89c2eb19055768a0cd8718d
gshshshvq <131294642+gshshshvq@users.noreply.github.com>
2023-05-13 17:32:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ load_dotenv()
 if __name__ == "__main__":
     app = Client(
         ":memory:",
-        api_id=os.environ.get("API_ID"),
-        api_hash=os.environ.get("API_HASH"),
-        bot_token=os.environ.get("BOT_TOKEN"),
+        api_id=os.environ.get("23843216"),
+        api_hash=os.environ.get("6f5e604a5082cbe15df96042882ac8ad"),
+        bot_token=os.environ.get("6222398027:AAHQwGGcTEISfthFVVmLgc4rP-gdMQWhwjg"),
         plugins=dict(root="modules"),
     )
     app.run()
